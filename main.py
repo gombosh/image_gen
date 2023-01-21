8d7e351e913ac3da1ec05a62737e8200dfae0435
gombosh <gombosh@gmail.com>
2023-01-21 22:03:39 +0200

    added req file

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import openai
 import os
 openai.api_key = os.getenv("MY_OPENAI_API")
 response = openai.Image.create(
-  prompt="photorealistic onion that looks like a barbie doll",
-  n=4,
+  prompt="cofee cup with earsphones on",
+  n=1,
   size="256x256"
 )
 for image in response['data']:
